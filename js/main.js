a1b1c6f806b3a488a813ff8bddf4360f58188f1e
Jeshua Maxey <jeshuamaxey@gmail.com>
2013-06-08 17:56:26 +0100

    added twitter bootstrap styles and added text increase/decrease voice commands

--- js/main.js
+++ js/main.js
@@ -114,17 +114,47 @@ function interpret(command) {
     command = command.substr(1,command.length);
   }
   switch(command.toLowerCase()) {
+    //change bg colour
     case 'red':
     case 'green':
     case 'blue':
     case 'yellow':
     case 'white':
     case 'black':
-      $('body').css('background-color', command);
+      $('.wrapper').css('background-color', command);
+      break;
+    //increase text size
+    case 'increase text':
+    case 'increase text size':
+    case 'increase font size':
+    case 'make text bigger':
+    case 'make the text bigger':
+    case 'make the writing bigger':
+      increaseTextSize();
+      break;
+    //decrease text size
+    case 'decrease text':
+    case 'decrease text size':
+    case 'decrease font size':
+    case 'make text smaller':
+    case 'make the text smaller':
+    case 'make the writing smaller':
+      decreaseTextSize();
       break;
     default:
       alert('I\'m sorry I didn\'t understand that command');
   }
+}
+function increaseTextSize() {
+  //multiply by 1 to make the value a number, increase this number then append 'px'
+  var newTextSize = $('body').css('font-size').substr(0,$('body').css('font-size').length - 2)*1 + 4 + 'px';
+  $('body').css('font-size', newTextSize);
+}
+
+function decreaseTextSize() {
+  //multiply by 1 to make the value a number, decrease this number then append 'px'
+  var newTextSize = $('body').css('font-size').substr(0,$('body').css('font-size').length - 2)*1 - 4 + 'px';
+  $('body').css('font-size', newTextSize);
 }
 
 /*
